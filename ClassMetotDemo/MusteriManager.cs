1c926b980ad0e09b8834fe266583c2b2a3d43c03
Selim Yüksel <yukselselim26@hotmail.com>
2021-01-20 21:17:12 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi : " + musteri.MusteriSoyadi + musteri.MusteriAdi);
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi : " + musteri.MusteriSoyadi + musteri.MusteriAdi);
+        }                         
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAdi = "Handan";
+            musteri1.MusteriSoyadi = "Alper";
+            
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAdi = "Selim";
+            musteri2.MusteriSoyadi = "Yüksel";
+            
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.MusteriAdi);
+                Console.WriteLine(musteri.MusteriSoyadi);
+                Console.WriteLine("-----------");
+                   
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriEkle(musteri1);            
+            musteriManager.MusteriSil(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriSil(musteri2);
+
+
+        }
+    }
+}
